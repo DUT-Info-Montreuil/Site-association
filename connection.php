e04d23549a84246e4c2ceedf92b043ddedc43f0e
Tarroook <tarook.zinou@gmail.com>
2022-10-19 14:31:28 +0200

    fixed connection

--- connection.php
+++ connection.php
@@ -1,16 +1,17 @@
 <?php
     class Connection{
         // enter in terminal before modifying so that you don't commit variables : git update-index --skip-worktree connection.php 
-        private $host = "database-etudiants.iut.univ-paris8.fr";
-        private $user = "dutinfopw201646";
-        private $pass = "bygyjyjy";
+        // enter in terminal after modifying so that you can commit variables : git update-index --no-skip-worktree connection.php
+        private static $host = "database-etudiants.iut.univ-paris8.fr";
+        private static $user = "dutinfopw201646";
+        private static $pass = "bygyjyjy";
         public static $db;
         
         public static function connect(){
             self::$db= new PDO(
-                'mysql:dbname=' . $this->user . ';host=' . $this->host,
-                $this->user,
-                $this->pass
+                'mysql:dbname=' . self::$user . ';host=' . self::$host,
+                self::$user,
+                self::$pass
             );
         }
     }
