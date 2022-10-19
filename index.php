daf46f8f9593359af2afb640168f937e1196633c
Tarroook <tarook.zinou@gmail.com>
2022-10-19 10:16:56 +0200

    Added empty connection classes

--- index.php
+++ index.php
@@ -4,5 +4,14 @@
 		<META CHARSET = 'UTF - 8'/>
 		<TITLE> Site association </TITLE>
 	</HEAD>
+
+    <BODY>
+        <HEADER>
+            <H1> Site association </H1>
+        </HEADER>
+        <FOOTER>
+            <H2> wow </H2>
+        </FOOTER>
+    </BODY>
     ";
 ?>
