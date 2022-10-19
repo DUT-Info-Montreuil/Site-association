441f072d7052d9501b56e5fc030594ea86d570bb
Tarroook <tarook.zinou@gmail.com>
2022-10-19 15:14:59 +0200

    updated connection

--- connection.php
+++ connection.php
@@ -2,9 +2,9 @@
     class Connection{
         // enter in terminal before modifying so that you don't commit variables : git update-index --skip-worktree connection.php 
         // enter in terminal after modifying so that you can commit variables : git update-index --no-skip-worktree connection.php
-        private static $host = "database-etudiants.iut.univ-paris8.fr";
-        private static $user = "dutinfopw201646";
-        private static $pass = "bygyjyjy";
+        private static $host = "host";
+        private static $user = "user";
+        private static $pass = "password";
         public static $db;
         
         public static function connect(){
