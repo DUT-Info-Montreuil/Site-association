9974be23b369eed42639d625163493715ef17aff
Tarroook <tarook.zinou@gmail.com>
2022-10-19 11:59:24 +0200

    Added connection to index

--- connection.php
+++ connection.php
@@ -1,12 +1,12 @@
 <?php
     class Connection{
         // enter in terminal before modifying so that you don't commit variables : git update-index --skip-worktree connection.php 
-        private $host = "host";
-        private $user = "user";
-        private $pass = "password";
+        private $host = "database-etudiants.iut.univ-paris8.fr";
+        private $user = "dutinfopw201646";
+        private $pass = "bygyjyjy";
         public static $db;
         
-        public static function initConnexion(){
+        public static function connect(){
             self::$db= new PDO(
                 'mysql:dbname=' . $this->user . ';host=' . $this->host,
                 $this->user,
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+    include_once "connection.php";
+    Connection::connect();
     echo "
     <HEAD>
 		<META CHARSET = 'UTF - 8'/>
