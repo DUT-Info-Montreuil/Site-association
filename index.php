10f3c7b7e05640d196fe668d130c4b503aee3f06
Tarroook <tarook.zinou@gmail.com>
2022-10-26 15:43:00 +0200

    Moved main html to template.php

--- index.php
+++ index.php
@@ -21,28 +21,7 @@
         $modConnection = new ModConnection();
     }
     $modConnection->saveDisplay();
+
+
+    require_once "template.php";
 ?>
-
-<HTML>
-    <HEAD>
-		<META CHARSET = 'UTF - 8'/>
-		<TITLE> Site association </TITLE>
-        <link href='style.css' rel='stylesheet' type='text/css'>
-        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi' crossorigin='anonymous'>
-        <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js' integrity='sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3' crossorigin='anonymous'></script>
-	</HEAD>
-
-    <BODY>
-        <HEADER>
-            <H1> Site association </H1>
-        </HEADER>
-        <MAIN>
-            <?php
-                echo $modConnection->getDisplay();
-            ?>
-        </MAIN>
-        <FOOTER>
-            <H2> wow </H2>
-        </FOOTER>
-    </BODY>
-</HTML>
