6374f606a985818775492904c50853ddd1c6f9da
Lucas Astier <parukia77@gmail.com>
2022-11-08 15:07:27 +0100

    The inputs in the requests are now put in the database

--- modules/request/model_request.php
+++ modules/request/model_request.php
@@ -3,7 +3,7 @@
         public function __construct(){}
         
         public function submitRequest($title, $description){
-            $query = self::$db->prepare("INSERT INTO request(title, description) VALUES (:title, :description");
+            $query = self::$db->prepare("INSERT INTO request (title, description) VALUES (:title, :description)");
             $query->execute(array(":title" => $title, ":description" => $description));
         }
     }
