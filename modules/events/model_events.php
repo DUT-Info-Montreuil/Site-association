eacf5b4830cf85d064bb8d0fddc67e2e0d74b438
Tarroook <tarook.zinou@gmail.com>
2022-11-09 09:25:28 +0100

    Created basic files for events page

---
+++ modules/events/model_events.php
@@ -0,0 +1,5 @@
+<?php
+    class modelEvents extends Connection{
+        
+    }
+?>
---
+++ modules/events/view_events.php
@@ -0,0 +1,7 @@
+<?php
+    class ViewEvents extends GenericView{
+        public function __construct(){
+            parent::__construct();
+        }
+    }
+?>
