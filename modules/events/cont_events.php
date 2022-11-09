451fc699f4f7a3a0bc2279840d59ca648a1041dd
Tarroook <tarook.zinou@gmail.com>
2022-11-09 11:53:21 +0100

    made page openable

--- modules/events/cont_events.php
+++ modules/events/cont_events.php
@@ -2,6 +2,14 @@
     include_once 'modules/events/view_events.php';
     include_once 'modules/events/model_events.php';
     class ContEvents{
+        public $model;
+        public $view;
+        public $action;
 
+        public function __construct(){
+            isset($_GET['action']) ? $this->action = $_GET['action'] : $this->action = 'default';
+            $this->model = new ModelEvents();
+            $this->view = new ViewEvents();
+        }
     }
 ?>
--- modules/events/mod_events.php
+++ modules/events/mod_events.php
@@ -1,6 +1,18 @@
 <?php
     include_once 'modules/events/cont_events.php';
     class ModEvents{
-        
+        public $cont;
+
+        public function __construct(){
+            $this->cont = new ContEvents;
+        }
+     
+        public function saveDisplay(){
+            $this->cont->view->setDisplay();
+        }
+
+        public function getDisplay(){
+            return $this->cont->view->getDisplay();
+        }
     }
 ?>
