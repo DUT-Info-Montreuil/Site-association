fba340970edfa0c044c464eaf74f6cd2e8d08b49
Tarroook <tarook.zinou@gmail.com>
2022-11-29 12:08:16 +0100

    Added button + removed useless method

--- modules/events/view_events.php
+++ modules/events/view_events.php
@@ -24,10 +24,12 @@
                     if(isset($_SESSION['id']) && $event['creatorId'] == $_SESSION ['id']){
                         echo "Vous";
                     } else{
-                        echo Connection::getUserFromId($event['creatorId'])['username']; 
+                        echo Connection::getUserDataFromId($event['creatorId'])['username']; 
                     }?></p>
                 </div>
+
                 <p class="eventDesc"><?php echo $event['description']; ?></p>
+
                 <div class="eventBottom">
                     <p>du : <?php echo $event['startDate']; ?></p>
                     <p>au : <?php echo $event['endDate']; ?></p>
@@ -36,10 +38,21 @@
                     } ?> participants
                     </p>
                 </div>
+
                 <?php
+                    if(isset($_SESSION['id']) && $event['creatorId'] != $_SESSION ['id']){
+                        // button to participate to the event
+                        ?>
+                        <button class="btn btn-success" id="subcribeEventButton" onclick="subscribeToEvent(<?php echo $event['id']?>)">
+                            Participer
+                        </button>
+                <?php
+                    }
+                    
+
                     if(isset($_SESSION['id']) && $event['creatorId'] == $_SESSION ['id']){
                         //ajax buttons to delete or modify the event
-                        ?>
+                ?>
                         <div class="eventButtons">
                             <button class="btn btn-danger" onclick="deleteEvent(<?php echo $event['id']; ?>)"><img src="resources/Trashcan.png"></button>
                             <button class="btn btn-warning" onclick="modifyEvent(<?php echo $event['id']; ?>)"><img src="resources/Craft.png"></button>
